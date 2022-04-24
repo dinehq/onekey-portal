fd52422a557d6c2663e6c59145e608e8f2ec5975
limichange <limichange@hotmail.com>
2022-04-24 13:28:25 +0800

    feat: add font StabilGroteskTrial

---
+++ src/components/base/Flex/Flex.tsx
@@ -0,0 +1,50 @@
+import { FC, ReactNode } from 'react';
+
+export interface FlexProps {
+  children?: ReactNode;
+  direction?: 'row' | 'row-reverse' | 'column' | 'column-reverse';
+  wrap?: 'nowrap' | 'wrap' | 'wrap-reverse';
+  gap?: number;
+  justifyContent?:
+    | 'flex-start'
+    | 'flex-end'
+    | 'center'
+    | 'space-between'
+    | 'space-around'
+    | 'space-evenly';
+  alignContent?:
+    | 'flex-start'
+    | 'flex-end'
+    | 'center'
+    | 'space-between'
+    | 'space-around'
+    | 'space-evenly';
+  alignItems?: 'flex-start' | 'flex-end' | 'center' | 'stretch' | 'baseline';
+}
+
+export const Flex: FC<FlexProps> = (props) => {
+  const {
+    children,
+    direction,
+    justifyContent,
+    alignContent,
+    alignItems,
+    gap,
+    ...omittedProps
+  } = props;
+
+  const innerStyle = {
+    display: 'flex',
+    flexDirection: direction,
+    justifyContent,
+    alignContent,
+    alignItems,
+    gap,
+  };
+
+  return (
+    <div css={innerStyle} {...omittedProps}>
+      {children}
+    </div>
+  );
+};
--- src/components/base/index.ts
+++ src/components/base/index.ts
@@ -1,3 +1,4 @@
-export * from './Footer';
 export * from './DevelopmentWrap';
+export * from './Flex';
+export * from './Footer';
 export * from './Nav';
