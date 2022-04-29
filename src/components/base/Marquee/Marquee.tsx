0ef3af871d297f267046757cdbfe4c16ac968790
limichange <limichange@hotmail.com>
2022-04-29 11:47:56 +0800

    feat: add Marquee component

---
+++ src/components/base/Marquee/Marquee.tsx
@@ -0,0 +1,47 @@
+import { Children, FC, ReactNode, useEffect, useState } from 'react';
+
+import { keyframes } from '@emotion/react';
+
+export interface MarqueeProps {
+  children?: ReactNode;
+}
+
+export const Marquee: FC<MarqueeProps> = (props) => {
+  const { children } = props;
+  const [animation, setAnimation] = useState('');
+
+  const innerChildren = Children.map(
+    [children, children, children],
+    (child) => {
+      if (child) {
+        return child;
+      }
+      return null;
+    },
+  );
+
+  useEffect(() => {
+    const bounce = keyframes`
+    0% {
+      transform: translate3d(-200%, 0, 0) rotate(0.001deg);
+    }
+    100% {
+      transform: translate3d(-100%, 0, 0) rotate(0.001deg);
+    }
+  `;
+
+    setAnimation(`${bounce} 50s linear infinite`);
+  }, []);
+
+  return (
+    <div
+      css={{
+        display: 'flex',
+        animation,
+        animationPlayState: 'running',
+      }}
+    >
+      {innerChildren}
+    </div>
+  );
+};
--- src/components/base/index.ts
+++ src/components/base/index.ts
@@ -4,3 +4,4 @@ export * from './Flex';
 export * from './Footer';
 export * from './Nav';
 export * from './Logo';
+export * from './Marquee';
