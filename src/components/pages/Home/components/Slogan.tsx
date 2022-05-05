3865e05f05438c4ca3be07655e82f24cf0ab30e4
limichange <limichange@hotmail.com>
2022-05-05 16:46:46 +0800

    feat: Big title add fadein animation

--- src/components/pages/Home/components/Slogan.tsx
+++ src/components/pages/Home/components/Slogan.tsx
@@ -1,10 +1,44 @@
 import { FC } from 'react';
 
+import { motion } from 'framer-motion';
+
 import { Box, Logo } from '../../../base';
 
+const variants = {
+  open: {
+    transition: { staggerChildren: 0.2, delayChildren: 0.8 },
+  },
+  closed: {
+    transition: { staggerChildren: 0.05, staggerDirection: -1 },
+  },
+};
+
+const itemVariants = {
+  open: {
+    display: 'block',
+    y: 0,
+    opacity: 1,
+    transition: { duration: 0.85 },
+  },
+  closed: {
+    y: 50,
+    opacity: 0,
+    transition: { duration: 0.85 },
+  },
+};
+
 export const Slogan: FC = () => (
-  <Box padding={51}>
-    <p
+  <Box
+    css={{
+      paddingTop: '10vh',
+      paddingBottom: '10vh',
+    }}
+  >
+    <motion.p
+      viewport={{ once: true }}
+      variants={variants}
+      whileInView="open"
+      initial="closed"
       css={{
         margin: '0 auto',
         fontSize: `${(60 / 1400) * 100}vw`,
@@ -13,18 +47,17 @@ export const Slogan: FC = () => (
         textAlign: 'center',
       }}
     >
-      <Logo
-        css={{
-          width: `${(64 / 1400) * 100}vw`,
-          height: `${(64 / 1400) * 100}vw`,
-          display: 'inline',
-          verticalAlign: 'bottom',
-          color: '#2EDB43',
-          marginRight: 6,
-        }}
-      />
-
-      <span>
+      <motion.span variants={itemVariants}>
+        <Logo
+          css={{
+            width: `${(64 / 1400) * 100}vw`,
+            height: `${(64 / 1400) * 100}vw`,
+            display: 'inline',
+            verticalAlign: 'bottom',
+            color: '#2EDB43',
+            marginRight: 6,
+          }}
+        />
         <span
           css={{
             color: '#2EDB43',
@@ -32,9 +65,14 @@ export const Slogan: FC = () => (
         >
           OneKey
         </span>{' '}
-        is the smartest way <br /> to secure, buy, exchange and <br /> grow your
-        crypto assets.
-      </span>
-    </p>
+        is the smartest way
+      </motion.span>
+      <motion.span variants={itemVariants}>
+        to secure, buy, exchange and{' '}
+      </motion.span>
+      <motion.span variants={itemVariants}>
+        grow your crypto assets.
+      </motion.span>
+    </motion.p>
   </Box>
 );
