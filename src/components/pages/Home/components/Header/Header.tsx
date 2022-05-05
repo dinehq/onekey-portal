9f60b2ecabd722fafbb360494bfff7686b3346ea
limichange <limichange@hotmail.com>
2022-05-05 15:38:56 +0800

    feat: clear code

--- src/components/pages/Home/components/Header/Header.tsx
+++ src/components/pages/Home/components/Header/Header.tsx
@@ -25,58 +25,59 @@ export const Header: FC = () => {
   const allRef = mergeRefs(borderRadiusRef, paddingRef);
 
   return (
-    <div ref={allRef}>
-      <motion.div style={{ height: '100vh', padding: paddingMotionValue }}>
-        <motion.div
-          style={{
-            boxShadow: '0px -6px 40px rgba(0, 0, 0, 0.08)',
+    <motion.div
+      ref={allRef}
+      style={{ height: '100vh', padding: paddingMotionValue }}
+    >
+      <motion.div
+        style={{
+          boxShadow: '0px -6px 40px rgba(0, 0, 0, 0.08)',
+          height: '100%',
+          borderRadius: borderRadiusMotionValue,
+          overflow: 'hidden',
+        }}
+      >
+        <Box
+          {...{
             height: '100%',
-            borderRadius: borderRadiusMotionValue,
-            overflow: 'hidden',
+            paddingLeft: 64,
+            paddingRight: 64,
+            paddingTop: 78,
+            paddingBottom: 78,
+            backgroundColor: '#e9e9e9',
+            display: 'flex',
+            alignItems: 'flex-end',
+            label: 'Headline',
+            backgroundImage: `url(${backgroundImage.toString()})`,
+            backgroundSize: 'cover',
+            backgroundRepeat: 'no-repeat',
+            backgroundPosition: 'center',
           }}
         >
-          <Box
-            {...{
-              height: '100%',
-              paddingLeft: 64,
-              paddingRight: 64,
-              paddingTop: 78,
-              paddingBottom: 78,
-              backgroundColor: '#e9e9e9',
-              display: 'flex',
-              alignItems: 'flex-end',
-              label: 'Headline',
-              backgroundImage: `url(${backgroundImage.toString()})`,
-              backgroundSize: 'cover',
-              backgroundRepeat: 'no-repeat',
-              backgroundPosition: 'center',
-            }}
-          >
-            <Flex {...{ flexDirection: 'column', gap: 20 }}>
-              <h1
-                css={{
-                  fontSize: 48,
-                  fontWeight: 600,
-                  lineHeight: '60px',
-                  color: '#101111',
-                }}
-              >
-                All-in-One Crypto Wallet.
-                <br />
-                Trusted by Millions.
-              </h1>
-              <Flex {...{ gap: 20 }}>
-                <Button>Launch App</Button>
-                <Button variant="outline">Go to shop</Button>
-              </Flex>
-
-              <span css={{ fontSize: 12, color: '#101111' }}>
-                Trustpilot score & review
-              </span>
+          <Flex {...{ flexDirection: 'column', gap: 20 }}>
+            <h1
+              css={{
+                fontSize: 48,
+                fontWeight: 600,
+                lineHeight: '60px',
+                color: '#101111',
+              }}
+            >
+              All-in-One Crypto Wallet.
+              <br />
+              Trusted by Millions.
+            </h1>
+            <Flex {...{ gap: 20 }}>
+              <Button>Launch App</Button>
+              <Button variant="outline">Go to shop</Button>
             </Flex>
-          </Box>
-        </motion.div>
+
+            <span css={{ fontSize: 12, color: '#101111' }}>
+              Trustpilot score & review
+            </span>
+          </Flex>
+        </Box>
       </motion.div>
-    </div>
+    </motion.div>
   );
 };
