cbf44419c2fc456d18d18d2dbadc951b1317eaf0
limichange <limichange@hotmail.com>
2022-05-06 17:16:01 +0800

    feat: export section component

--- src/components/base/Box/index.ts
+++ src/components/base/Box/index.ts
@@ -1,1 +1,13 @@
+import { FC } from 'react';
+
+import { jsx } from '@emotion/react';
+
+import { Box, BoxProps } from './Box';
+
 export * from './Box';
+
+export const Section: FC<BoxProps> = (props) =>
+  jsx(Box, {
+    as: 'section',
+    ...props,
+  });
