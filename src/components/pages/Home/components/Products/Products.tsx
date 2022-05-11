b0f89e4413deb0eb180e4b3dafaf0e0d61bae7d7
limichange <limichange@hotmail.com>
2022-05-11 17:03:05 +0800

    feat: update Products style

--- src/components/pages/Home/components/Products/Products.tsx
+++ src/components/pages/Home/components/Products/Products.tsx
@@ -1,11 +1,20 @@
 import { FC } from 'react';
 
+import { useTheme } from '@emotion/react';
 import { useTransform } from 'framer-motion';
 
-import { useElementInViewportProgress } from '../../../../../hooks/useElementInViewportProgress';
-import { Box, Button, CanvasPlayer, Container } from '../../../../base';
+import { useElementInViewportProgress } from '../../../../../hooks';
+import {
+  Box,
+  Button,
+  CanvasPlayer,
+  Container,
+  Section,
+  Span,
+} from '../../../../base';
 
 export const Products: FC = () => {
+  const theme = useTheme();
   const { ref, elementInViewportProgress } = useElementInViewportProgress(0);
   const motionValue = useTransform(
     elementInViewportProgress,
@@ -14,85 +23,100 @@ export const Products: FC = () => {
   );
 
   return (
-    <Container>
-      <div
-        css={{
-          paddingLeft: 87,
-          paddingRight: 87,
-          paddingTop: 87,
-          paddingBottom: 87,
-          display: 'flex',
-          alignItems: 'center',
-          label: 'Frame 67',
-          position: 'relative',
-        }}
-      >
-        <div css={{ display: 'flex', gap: 200, flexGrow: 1 }}>
-          <div css={{ display: 'flex', gap: 24 }}>
-            <div css={{ display: 'flex', flexDirection: 'column', gap: 336 }}>
-              <div css={{ display: 'flex', flexDirection: 'column', gap: 20 }}>
+    <Section>
+      <Container>
+        <div
+          css={{
+            paddingLeft: 87,
+            paddingRight: 87,
+            paddingTop: 87,
+            paddingBottom: 87,
+            display: 'flex',
+            alignItems: 'center',
+            position: 'relative',
+          }}
+        >
+          <div css={{ display: 'flex', gap: 200, flexGrow: 1 }}>
+            <div css={{ display: 'flex', gap: 24 }}>
+              <div css={{ display: 'flex', flexDirection: 'column', gap: 336 }}>
                 <div
-                  css={{ display: 'flex', flexDirection: 'column', gap: 16 }}
+                  css={{ display: 'flex', flexDirection: 'column', gap: 24 }}
                 >
-                  <span css={{ fontSize: 36, color: '#101111' }}>
-                    Advanced on-chain wallet app
-                  </span>
-                  <span css={{ fontSize: 16, color: '#313638' }}>
-                    Use the OneKey App separately to store and use your crypto
-                    assets in a secure and convenient manner. Each user's assets
-                    are secured appropriately and stored locally.
-                  </span>
+                  <div
+                    css={{ display: 'flex', flexDirection: 'column', gap: 16 }}
+                  >
+                    <Span css={{ ...theme.text.medium900 }}>
+                      Advanced on-chain wallet app
+                    </Span>
+                    <Span css={{ ...theme.text.normal400 }}>
+                      Use the OneKey App separately to store and use your crypto
+                      assets in a secure and convenient manner. Each user's
+                      assets are secured appropriately and stored locally.
+                    </Span>
+                  </div>
+                  <Box>
+                    <Button>Launch App</Button>
+                  </Box>
                 </div>
-                <Button>Launch App</Button>
-              </div>
-              <div css={{ display: 'flex', flexDirection: 'column', gap: 20 }}>
-                <div css={{ display: 'flex', flexDirection: 'column', gap: 8 }}>
-                  <span css={{ fontSize: 36, color: '#101111' }}>
-                    Need more security? No problem.
-                  </span>
-                  <span css={{ fontSize: 16, color: '#313638' }}>
-                    Hardware wallets take the securitisation of digital assets
-                    to another level. From physically isolating hackers to
-                    preventing Trojan horses and software attacks. All transfers
-                    must be approved by the hardware wallet, so you don't have
-                    to worry about the loss or theft of your mobile phone.
-                  </span>
+
+                <div
+                  css={{ display: 'flex', flexDirection: 'column', gap: 24 }}
+                >
+                  <div
+                    css={{ display: 'flex', flexDirection: 'column', gap: 8 }}
+                  >
+                    <Span css={{ ...theme.text.medium900 }}>
+                      Need more security? No problem.
+                    </Span>
+
+                    <Span css={{ ...theme.text.normal400 }}>
+                      Hardware wallets take the securitisation of digital assets
+                      to another level. From physically isolating hackers to
+                      preventing Trojan horses and software attacks. All
+                      transfers must be approved by the hardware wallet, so you
+                      don't have to worry about the loss or theft of your mobile
+                      phone.
+                    </Span>
+                  </div>
+
+                  <Box>
+                    <Button variant="outline">Go to shop</Button>
+                  </Box>
                 </div>
-                <Button variant="outline">Go to shop</Button>
               </div>
             </div>
-          </div>
 
-          <div ref={ref}>
-            <Box
-              css={{
-                height: '100vh',
-                position: 'relative',
-              }}
-            >
+            <div ref={ref}>
               <Box
                 css={{
-                  top: `calc(50% - ${502 / 2}px)`,
-                  position: 'sticky',
-                  width: 644,
-                  height: 502,
-                  borderRadius: 24,
-                  overflow: 'hidden',
+                  height: '100vh',
+                  position: 'relative',
                 }}
               >
-                <CanvasPlayer
-                  width={1288 / 2}
-                  height={1004 / 2}
-                  images={new Array(60)
-                    .fill(0)
-                    .map((_, i) => `/canvas/image${i}.jpg`)}
-                  frame={parseInt(motionValue.get().toFixed(0))}
-                />
+                <Box
+                  css={{
+                    top: `calc(50% - ${502 / 2}px)`,
+                    position: 'sticky',
+                    width: 644,
+                    height: 502,
+                    borderRadius: 24,
+                    overflow: 'hidden',
+                  }}
+                >
+                  <CanvasPlayer
+                    width={1288 / 2}
+                    height={1004 / 2}
+                    images={new Array(60)
+                      .fill(0)
+                      .map((_, i) => `/canvas/image${i}.jpg`)}
+                    frame={parseInt(motionValue.get().toFixed(0))}
+                  />
+                </Box>
               </Box>
-            </Box>
+            </div>
           </div>
         </div>
-      </div>
-    </Container>
+      </Container>
+    </Section>
   );
 };
