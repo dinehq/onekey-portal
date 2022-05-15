84592b8ac0d171ff7285e668c01264f89b5ebe78
limichange <limichange@hotmail.com>
2022-05-15 16:57:40 +0800

    feat: add menu data

---
+++ src/components/base/PageFooter/usePageFooterData.ts
@@ -0,0 +1,151 @@
+// Product
+// Crowdfunding
+// Mobile Wallet
+// Hardware Device
+// Browser Extension
+// Swap
+// Token List
+// Recovery Phrase Converter
+
+// Resource
+// System Status
+// Open Source Community
+// Media Kits
+// Privacy Agreement
+// User Agreement
+
+// Information
+// Submit Dapp
+// Submit Token
+// Venture Capital
+// Sales Alliance
+// Advertising
+// Investor Relations
+
+// Crypto Assets
+// Bitcoin (BTC)
+// Ethereum (ETH)
+// Litecoin (LTC)
+// Dogecoin (DOGE)
+// Dash (DASH)
+
+export function usePageFooterData() {
+  return [
+    {
+      name: 'product',
+      link: '',
+      list: [
+        {
+          name: 'Crowdfunding',
+          link: '/crowdfunding',
+        },
+        {
+          name: 'Mobile Wallet',
+          link: '/mobile-wallet',
+        },
+        {
+          name: 'Hardware Device',
+          link: '/hardware-device',
+        },
+        {
+          name: 'Browser Extension',
+          link: '/browser-extension',
+        },
+        {
+          name: 'Swap',
+          link: '/swap',
+        },
+        {
+          name: 'Token List',
+          link: '/token-list',
+        },
+        {
+          name: 'Recovery Phrase Converter',
+          link: '/recovery-phrase-converter',
+        },
+      ],
+    },
+    {
+      name: 'resource',
+      link: '',
+      list: [
+        {
+          name: 'System Status',
+          link: '/system-status',
+        },
+        {
+          name: 'Open Source Community',
+          link: '/open-source-community',
+        },
+        {
+          name: 'Media Kits',
+          link: '/media-kits',
+        },
+        {
+          name: 'Privacy Agreement',
+          link: '/privacy-agreement',
+        },
+        {
+          name: 'User Agreement',
+          link: '/user-agreement',
+        },
+      ],
+    },
+    {
+      name: 'information',
+      link: '',
+      list: [
+        {
+          name: 'Submit Dapp',
+          link: '/submit-dapp',
+        },
+        {
+          name: 'Submit Token',
+          link: '/submit-token',
+        },
+        {
+          name: 'Venture Capital',
+          link: '/venture-capital',
+        },
+        {
+          name: 'Sales Alliance',
+          link: '/sales-alliance',
+        },
+        {
+          name: 'Advertising',
+          link: '/advertising',
+        },
+        {
+          name: 'Investor Relations',
+          link: '/investor-relations',
+        },
+      ],
+    },
+    {
+      name: 'crypto-assets',
+      link: '/crypto-assets',
+      list: [
+        {
+          name: 'Bitcoin (BTC)',
+          link: '/bitcoin',
+        },
+        {
+          name: 'Ethereum (ETH)',
+          link: '/ethereum',
+        },
+        {
+          name: 'Litecoin (LTC)',
+          link: '/litecoin',
+        },
+        {
+          name: 'Dogecoin (DOGE)',
+          link: '/dogecoin',
+        },
+        {
+          name: 'Dash (DASH)',
+          link: '/dash',
+        },
+      ],
+    },
+  ];
+}
